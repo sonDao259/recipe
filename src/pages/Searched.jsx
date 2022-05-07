e666c3f44882ebbcd231506899b6952a0cd3a55d
Dao Hoang Son <hoangson28122017@gmail.com>
2022-05-07 08:23:54 +0700

    fix

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -25,7 +25,7 @@ function Searched() {
                 {searchedRecipes.map(item => {
                     return (
                         <Card key={item.id}>
-                            <Link>
+                            <Link to={'/recipe/' + item.id}>
                                 <img src={item.image} alt='' />
                                 <h4>{item.title}</h4>
                             </Link>
